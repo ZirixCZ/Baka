7120293c676ffe537e04707d716ef7dd89d2a8cd
ZirixCZ <49836430+ZirixCZ@users.noreply.github.com>
2019-08-25 20:34:37 +0200

    General update

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
-
-
-
 const Discord = require("discord.js");
 const Token = require("./auth.json");
+
+client.on("ready", () => {
+    console.log("ready, dont close IT AND GO FIND DA HOSTING");})
+
+client.on("message", message => {
+     if (message[0] != ".")
+         return;
+            //commands will go here theoretically :DDD
+    
+    
+        
+});
 
